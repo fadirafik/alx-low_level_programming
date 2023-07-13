72e54a805ff6a3e08d30c58dcf11074a41ba7c0f
fadirafik <fadihadddad11@gmail.com>
2023-07-13 22:08:07 +0300

    f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints with proper grammar, but the outcome is a piece of art,
+ * main - prints "with proper grammar, but the outcome is 
+ * a piece of art," followed newline
  * Return: Always 0.
+ *
  */
 
 int main(void)
