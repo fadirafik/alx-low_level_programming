2bb67a40a5e43368955ab75245204365834958ee
fadirafik <fadihadddad11@gmail.com>
2023-07-13 21:19:15 +0300

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,15 @@
 #include <string.h>
 
 /**
-* this is the first thing i have to say about the main function
-* which is it prints hello world through the function 
-* puts
-*/
+ * this is the first thing i have to say about the main function
+ * which is it prints hello world through the function 
+ * puts
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
 	puts(str);
+
+	/* this is the first comment on the return */
 	return (0);
 }
