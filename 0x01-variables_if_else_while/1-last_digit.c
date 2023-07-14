4cce09d126932da96c3d7fe65f670fe02ba03809
fadirafik <fadihadddad11@gmail.com>
2023-07-14 22:29:48 +0300

    'edef'

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,8 @@
 * authored by fadi haddad
 */
 
-/*
-* main - figures out if the last digit of a nuber 
+/**
+* main - figures out if the last digit of a nuber
 * is greater or lesser than five and if it is 0
 * Return: Always 0 (Success)
 */
@@ -18,15 +18,15 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-        last = n % 10;
+	last = n % 10;
 
-	printf("Last digit of %d is %d ", n , last);
+	printf("Last digit of %d is %d ", n, last);
 	if (last > 5)
 		printf("and is greater than 5\n");
 	else if (last == 0)
 		printf("and is 0\n");
 	else
-		printf("and is less than 6 and not 0\n"); 
+		printf("and is less than 6 and not 0\n");
 
 	return (0);
 }
