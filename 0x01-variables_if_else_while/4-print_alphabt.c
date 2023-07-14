8acf12fdba5e05926b190fe9a8d03363ca643197
fadirafik <fadihadddad11@gmail.com>
2023-07-15 01:56:22 +0300

    'edef'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/*
+* this is me mario
+*/
+
+/**
+* main - prints the whole alphabet except e and q
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+char ch;
+char eo;
+eo = '\n';
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch == 'e' || ch == 'q')
+{
+  continue;
+}
+putchar (ch);
+}
+putchar (eo);
+return (0);
+}
