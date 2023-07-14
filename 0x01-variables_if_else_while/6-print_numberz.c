3e56d61197c15e0242a29d10da6482cc9ed584d2
fadirafik <fadihadddad11@gmail.com>
2023-07-15 02:22:45 +0300

    'edef'

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+* this is me mario
+*/
+
+/**
+* main - prints the whole decimal numbers
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch;
+	char eo;
+
+	eo = '\n';
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar (ch);
+	}
+	putchar (eo);
+	return (0);
+}
