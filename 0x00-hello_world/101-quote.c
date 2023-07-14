7e116331fb5d3262ae35136cb7ccfc4c6409067d
fadirafik <fadihadddad11@gmail.com>
2023-07-14 03:19:33 +0300

    d

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - prints and that piece of art is beautiful
+*
+* Return: Always 1 (Failure)
+*/
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n");
+	return (1);
