7c36650249e229e7329c62518547dc8e4d6e030d
fadirafik <fadihadddad11@gmail.com>
2023-07-15 05:01:43 +0300

    'thos'

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+/*
+* this is me mario
+*/
+
+/**
+* main - prints the whole decimal numbers
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+	int ch, nu;
+	int fj;
+	int hm;
+	int eo;
+	int com;
+	int sp;
+
+	eo = '\n';
+	sp = ' ';
+	com = ',';
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		for (nu = '0'; nu <= '9'; nu++)
+		{
+			for (fj = '0'; fj <= '9'; fj++)
+			{
+				for (hm = '0'; hm <= '9'; hm++)
+				{
+					if (hm > nu && fj >= ch)
+					{
+						putchar (ch);
+						putchar (nu);
+						putchar (sp);
+						putchar (fj);
+						putchar (hm);
+						if (hm == '9' && fj == '9' && nu == '8' && ch == '9')
+						break;
+						putchar (com);
+						putchar (sp);
+					}
+					else
+						continue;
+				}
+			}
+		}
+	}
+	putchar (eo);
+	return (0);
+}
