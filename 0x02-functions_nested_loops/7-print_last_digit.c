3ea328c77886450c48c261435cc08e5bb358b391
fadirafik <fadihadddad11@gmail.com>
2023-07-19 02:41:04 +0300

    'f'

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 10 || n > -10)
+	if (n > 10 || n < -10)
 	{
-		return (n);
+		return (10 % n);
 	}
 	else
 	{
-		return (10 % n);
+		return (n);
 	}
 }
