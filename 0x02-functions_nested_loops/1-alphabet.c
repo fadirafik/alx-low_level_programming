fd7acc2fe0b57838f282a2b9f46656c1604768dd
fadirafik <fadihadddad11@gmail.com>
2023-07-18 16:38:23 +0300

    s

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -14,21 +14,3 @@ int main(void)
 	print_alphabet();
 	return (0);
 }
-
-
-/**
-* print_alphabet - prints the whole alphabet
-* Return: Always 0 (success)
-*/
-void print_alphabet(void)
-{
-	int i;
-
-	i = 97;
-	while (i != 123)
-	{
-		putchar(i);
-		i++;
-	}
-	putchar('\n');
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,17 @@
-void print_alphabet(void);
+#include <stdio.h>
+/**
+* print_alphabet - prints the whole alphabet
+* Return: Always 0 (success)
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	i = 97;
+	while (i != 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+}
