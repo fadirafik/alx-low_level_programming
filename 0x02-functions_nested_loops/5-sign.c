ceff27530b002aeb51d5f673d7c2d59b86710c30
fadirafik <fadihadddad11@gmail.com>
2023-07-19 02:12:09 +0300

    'f'

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,16 +13,22 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		putchar('+');
+		putchar(',');
+		putchar(' ');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		putchar('-');
+		putchar(',');
+		putchar(' ');
 		return (-1);
 	}
 	else
 	{
 		putchar('0');
+		putchar(',');
+		putchar(' ');
 		return (0);
 	}
 }
