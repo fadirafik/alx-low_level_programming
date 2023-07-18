d6cedf65d390bb12be8fa2d93ded23bd64810cab
fadirafik <fadihadddad11@gmail.com>
2023-07-19 02:07:09 +0300

    'f'

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _isalpha - check the code.
+ * @n: is an entry of user
+ * Return: 1 if c is lower case
+ * 0 if otherwise
+ */
+int print_sign(int n);
+{
+	if (n > 0)
+	{
+		putchar('+')
+		return (1);
+	}
+	else if (n < 0)
+	{
+		putchar('-')
+		return (-1);
+	}
+	else
+	{
+		putcha('0')
+		return (0);
+	}
+}
