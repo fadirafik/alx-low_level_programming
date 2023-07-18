05536e2feca9cb5dd816a73b0d2e2e4783087c6d
fadirafik <fadihadddad11@gmail.com>
2023-07-19 01:43:08 +0300

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,17 @@
 * main - prints the string putchar
 * Return: Always 0 (success)
 */
-int _putchar(char c)
+int _putchar(char)
 {
-	return (write(1, &c, 1))
+	int i;
+	char f[] = "_putchar";
+
+	i = 0;
+	while (f[i] != '\0')
+	{
+		putchar(f[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
