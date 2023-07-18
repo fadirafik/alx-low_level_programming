5a8ae4c042b61b05048a25feaf9961ff8ce8e95f
fadirafik <fadihadddad11@gmail.com>
2023-07-19 02:29:37 +0300

    'f'

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * _abs - check the code.
+ * Return:Always 0 if otherwise
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
