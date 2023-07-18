196993da75f4e65989a6227ac365b4313c593669
fadirafik <fadihadddad11@gmail.com>
2023-07-18 16:49:16 +0300

    g

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /*
 * this is me mario
 */
 
 /**
-* main - prints the whole alphabet
+* print_alphabet - prints the whole alphabet
 * Return: Always 0 (success)
 */
-int main(void)
+void print_alphabet(void)
 {
-	print_alphabet();
-	return (0);
+	int i;
+
+	i = 97;
+	while (i != 123)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,17 +1,1 @@
-#include <stdio.h>
-/**
-* print_alphabet - prints the whole alphabet
-* Return: Always 0 (success)
-*/
-void print_alphabet(void)
-{
-	int i;
-
-	i = 97;
-	while (i != 123)
-	{
-		putchar(i);
-		i++;
-	}
-	putchar('\n');
-}
+void print_alphabet(void);
