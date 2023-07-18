f121a9364c6b05875264414dc8e791492dd38f90
fadirafik <fadihadddad11@gmail.com>
2023-07-19 01:45:39 +0300

    a

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,14 +4,15 @@
 
 /**
  * _islower - check the code.
- *
+ * c - is an entry of user
+ * 
  * Return: 1 if c is lower case .
  * 0 if otherwise
  */
 int _islower(int c)
 {
-    if (c >=97 && c <= 128)
-        return (1);
-    else
-        return (0);
+	if (c >= 97 && c <= 128)
+		return (1);
+	else
+		return (0);
 }
