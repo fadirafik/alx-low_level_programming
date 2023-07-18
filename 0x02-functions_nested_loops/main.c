1effe5a0f83027a813ccb39869be900276ef2840
fadirafik <fadihadddad11@gmail.com>
2023-07-19 01:34:23 +0300

    a

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,8 +1,13 @@
 #include "main.h"
+#include "3-islower.c"
 
-int main (void)
+int main(void)
+
 {
-    _islower(100);
-    return (0);
+    int r;
 
+    r = _islower('H');
+    r = _islower('o');
+    r = _islower(108);
+    return (0);
 }
