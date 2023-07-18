84b47db3a9190031160ce208aae2c4af778a78b2
fadirafik <fadihadddad11@gmail.com>
2023-07-19 02:57:50 +0300

    'f'

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,15 +6,15 @@
  * jack_bauer - counts every minute in a day
  * Return:Always 0 (succes)
  */
-int void jack_bauer(void)
+void jack_bauer(void)
 {
-    for (int i; i < 24 ; i++)
-    {
-        for (int j; j < 60 ; j++)
-        {
-            _putchar(i);
-            _putchar(':');
-            _putchar(j);
-        }
-    }
+	for (int i; i < 24 ; i++)
+	{
+		for (int j; j < 60 ; j++)
+		{
+			_putchar(i);
+			_putchar(':');
+			_putchar(j);
+		}
+	}
 }
