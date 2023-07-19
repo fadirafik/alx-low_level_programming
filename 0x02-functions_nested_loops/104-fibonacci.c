b4687217d5a0609f080968860907de0ef9d498fb
fadirafik <fadihadddad11@gmail.com>
2023-07-19 19:38:47 +0300

    'd'

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+
+/**
+ * main - counts every fibonnaci number
+ * Return:Always 0 (succes)
+ */
+int main(void)
+{
+	unsigned int small;
+	unsigned int big;
+	unsigned int tmp;
+	unsigned int sum;
+
+	small = 1;
+	big = 2;
+	while (big < 4000000)
+	{
+		if (big % 2 == 0)
+		{
+			sum += big;
+		}
+		tmp = big;
+		big = small + big;
+		small = tmp;
+
+	}
+	printf("%u\n", sum);
+	return (0);
+}
