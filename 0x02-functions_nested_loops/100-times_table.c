9d110037953f04082f9e882f98f49c4e85b26fe2
fadirafik <fadihadddad11@gmail.com>
2023-07-19 05:51:48 +0300

    'f'

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -28,6 +28,7 @@ void print_times_table(int n)
                     else
                     {
                         putchar(' ');
+                        putchar(' ');
                         putchar(k + '0');
                     }
                 }
@@ -39,6 +40,7 @@ void print_times_table(int n)
                 if (j != n)
                 {
                     putchar(',');
+                    putchar(' ');
                     putchar(' ');
                 }
         }
