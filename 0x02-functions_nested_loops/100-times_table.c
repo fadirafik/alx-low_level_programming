e1f2629bb7b33d819641a62cf2d5c3aa27bc8049
fadirafik <fadihadddad11@gmail.com>
2023-07-19 05:46:12 +0300

    'f'

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,43 @@
  * print_times_table - gives back multiplication table of n
  * @n: is the user input
  */
-void print_times_table(int n);
+void print_times_table(int n)
 {
-	int i;
-	int j;
-	int k;
+    int i;
+    int j;
+    int k;
 
-	if (n < 15 && n > 0)
-	{
-		for (i = 0; i < n ; i++)
-		{
-			for (j = 0; j < n ; j++)
-			{
-				k = j * i;
-				if (k < 10)
-				{
-					if (j == 0)
-					{
-						putchar(k + '0');
-					}
-					else
-					{
-						putchar(' ');
-						putchar(k + '0');
-					}
-				}
-				else
-				{
-					putchar((k / 10) + '0');
-					putchar((k % 10) + '0');
-				}
-				if (j != 9)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-		}
-			putchar('\n');
-		}
-	}
-	}
-
+    if (n <= 0 && n > 15)
+    {
+        for (i = 0; i < 10 ; i++)
+        {
+            for (j = 0; j < 10 ; j++)
+            {
+                k = j * i;
+                if (k < 10)
+                {
+                    if (j == 0)
+                    {
+                        putchar(k + '0');
+                    }
+                    else
+                    {
+                        putchar(' ');
+                        putchar(k + '0');
+                    }
+                }
+                else
+                {
+                    putchar((k / 10) + '0');
+                    putchar((k % 10) + '0');
+                }
+                if (j != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+        }
+            putchar('\n');
+        }
+    }
+}
