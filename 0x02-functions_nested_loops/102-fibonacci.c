9a2a2b7f82e354f8b815fd066a8fd580d1224494
fadirafik <fadihadddad11@gmail.com>
2023-07-19 18:57:45 +0300

    'f'

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -10,18 +10,25 @@ int main(void)
 {
 	int i;
 	long small;
-    long big;
-    long tmp;
+	long big;
+	long tmp;
 
 	small = 1;
-    big = 2;
-    printf("%ld, ", small);
+	big = 2;
+	printf("%ld, ", small);
 	for (i = 0; i < 49; i++)
 	{
-        printf("%ld, ", big);
-        tmp = big;
-        big = small + big;
-        small = tmp;
+		if (i == 48)
+		{
+			printf("%ld", big);
+		}
+		else
+		{
+			printf("%ld, ", big);
+			tmp = big;
+			big = small + big;
+			small = tmp;
+		}
 	}
 	printf("\n");
 	return (0);
