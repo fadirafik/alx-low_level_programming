0a70d6a25fc7ee9476b93133c013197637d5e830
fadirafik <fadihadddad11@gmail.com>
2023-07-19 18:29:42 +0300

    'f'

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,42 +8,38 @@
  */
 void print_times_table(int n)
 {
-    int i;
-    int j;
-    int k;
+	int num, mult, prod;
 
-    if (n >= 0 && n <= 15)
-    {
-        for (i = 0; i <= n ; i++)
-        {
+	if (n >= 0 && n <= 15)
+	{
+		for (num = 0; num <= n; num++)
+		{
+			_putchar('0');
 
-            for (j = 0; j <= n ; j++)
-            {
-                _putchar(',');
-                _putchar(' ');
+			for (mult = 1; mult <= n; mult++)
+			{
+				_putchar(',');
+				_putchar(' ');
 
-                k = j * i;
+				prod = num * mult;
 
-                if (k < 10)
-                {
-                    _putchar(' ');
-                }
-                if (k <= 99)
-                {
-                    _putchar(' ');
-                }
-                if (k > 99)
-                {
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10)) % 10 + '0');             
-                }
-                else if (k <= 99 && k >= 10)
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+
+				if (prod >= 100)
 				{
-					_putchar((k / 10) + '0');
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10)) % 10 + '0');
 				}
-				_putchar((k % 10) + '0');
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
+				_putchar((prod % 10) + '0');
 			}
 			_putchar('\n');
-        }    
-    }
+		}
+	}
 }
