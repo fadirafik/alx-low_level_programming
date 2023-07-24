52c9fcbf2539b379feabd985e70e45356c5be67e
fadirafik <fadihadddad11@gmail.com>
2023-07-25 01:55:38 +0300

    this is an automated commit

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,13 +14,17 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		if (i == (n - 1))
-			{
-				printf("%d\n", *(a + i));
-				break;
-			}
-		printf("%d, ", *(a + i));
+		for (i = 0; i < n; i++)
+		{
+			if (i == (n - 1))
+				{
+					printf("%d", *(a + i));
+					break;
+				}
+			printf("%d, ", *(a + i));
+		}
 	}
+	printf("\n");
 }
