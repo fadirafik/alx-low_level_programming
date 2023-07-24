027f199273cd6adb26bbdadab2a939325581c550
fadirafik <fadihadddad11@gmail.com>
2023-07-24 22:48:45 +0300

    this is an automated commit

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - resets a given number to 98
+ * @s: is the user input
+ *
+ * Return: string length
+*/
+void print_rev(char *s)
+{
+	unsigned long i;
+
+	for (i = strlen(s); i > 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
