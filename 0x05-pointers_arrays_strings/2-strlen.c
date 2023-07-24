095734f97f389250332185a28357aed7cf1296a8
fadirafik <fadihadddad11@gmail.com>
2023-07-24 22:32:01 +0300

    this is an automated commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+/**
+ * _strlen - resets a given number to 98
+ * @s: is the user input
+ *
+ * Return: string length
+*/
+
+int _strlen(char *s)
+{
+    int i;
+
+    i = strlen(s);
+    return (i);
+}
