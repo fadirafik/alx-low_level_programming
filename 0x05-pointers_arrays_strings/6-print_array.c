2e0b4fbc472239f8b19627939ae23781e45b8e94
fadirafik <fadihadddad11@gmail.com>
2023-07-25 01:42:44 +0300

    this is an automated commit

---
+++ 0x05-pointers_arrays_strings/6-print_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_array - prints every item of an array till
+ *  a given number n
+ * @a: is the user input
+ * @n: is the number to print to 
+ * 
+ * Return: void
+*/
+void print_array(int *a, int n);
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == (n - 1))
+			{
+				printf("%d\n", *(a + i));
+			}
+		printf("%d, ", *(a + i));
+	}
+}
