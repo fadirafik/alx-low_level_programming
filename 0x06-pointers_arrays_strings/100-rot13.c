b780bc94669cd3dc4fd385a76eab186e7d45015a
fadirafik <fadihadddad11@gmail.com>
2023-07-28 01:25:58 +0300

    this is an automated commit

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rot13 - changes specific characters to some numbers
+ * @s: string input
+ *
+ * Return: string
+ */
+char *rot13(char *s)
+{
+	int i, j;
+
+	char *c = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *d = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; c[j] != '\0'; j++)
+		{
+			if (s[i] == c[j])
+			{
+				s[i] = d[j];
+                break;
+			}
+
+		}
+	}
+
+	return (s);
+}
