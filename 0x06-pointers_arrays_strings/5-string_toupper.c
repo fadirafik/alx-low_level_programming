2c8de6c30b87bf50ea3190d6b243fe6c164d2779
fadirafik <fadihadddad11@gmail.com>
2023-07-27 21:09:26 +0300

    this is an automated commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * string_toupper - puts a string to uppercase
+ * @s: string input
+ *
+ * Return: string in uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+
+	while (s[i] != '\0')
+	{
+		if (s[i] > 96 && s[i] < 123)
+		{
+			s[i] =  s[i] - 32;
+			i++;
+		}
+		else
+		{
+			s[i] = s[i];
+			i++;
+		}
+	}
+	return (s);
+}
