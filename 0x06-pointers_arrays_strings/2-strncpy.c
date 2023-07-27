95c4a89ab88b53655aac11107b5e6db2831750a1
fadirafik <fadihadddad11@gmail.com>
2023-07-27 03:22:47 +0300

    this is an automated commit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies two strings
+ * @dest: string to append to
+ * @src: string to add
+ * @n: specific number of digits to append from source
+ *
+ * Return: a pointer to the resulting string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int l = 0;
+
+	while (l < n && src[l] != '\0')
+	{
+		dest[l] = src[l];
+		l++;
+	}
+	return (dest);
+}
