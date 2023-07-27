5dcc374d6fd33dfb959333187bf9557007f104ea
fadirafik <fadihadddad11@gmail.com>
2023-07-27 21:55:17 +0300

    this is an automated commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all the words in a string
+ * @s: string input
+ *
+ * Return: string in uppercase
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i - 1] < 65 || s[i - 1] > 122)
+		{
+			if (s[i] > 96 && s[i] < 123)
+			{
+				s[i] =  s[i] - 32;
+				i++;
+			}
+			else
+			{
+				s[i] = s[i];
+				i++;
+			}
+		}
+		else
+		{
+			i++;
+		}
+	}
+	return (s);
+}
