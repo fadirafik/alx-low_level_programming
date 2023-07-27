d9f43995d201e1f7e0714e93fdf9c242a1dbf71f
fadirafik <fadihadddad11@gmail.com>
2023-07-28 00:07:34 +0300

    this is an automated commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,16 +14,14 @@ char *leet(char *s)
 	{
 		if (s[i] == 'A' || s[i] == 'a')
 			s[i] = '4';
-		else if (s[i] == 'E' || s[i] == 'e')
+		if (s[i] == 'E' || s[i] == 'e')
 			s[i] = '3';
-		else if (s[i] == 'O' || s[i] == 'o')
+		if (s[i] == 'O' || s[i] == 'o')
 			s[i] = '0';
-		else if (s[i] == 'T' || s[i] == 't')
+		if (s[i] == 'T' || s[i] == 't')
 			s[i] = '7';
-		else if (s[i] == 'L' || s[i] == 'l')
+		if (s[i] == 'L' || s[i] == 'l')
 			s[i] = '1';
-		else
-			s[i] = s[i];
 		i++;
 	}
 	return (s);
