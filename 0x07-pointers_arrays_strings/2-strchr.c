770fa22b65acd247710d574288e43490d09cd1ea
fadirafik <fadihadddad11@gmail.com>
2023-07-31 20:43:19 +0300

    this is an automated commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a specific character in a string
+ * @s: the user input string
+ * @c: the character to be searched for
+ *
+ * Return: pointer to the character c in the string s
+ * or null if nothing is found
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+		i++;
+	}
+	return ("");
+}
