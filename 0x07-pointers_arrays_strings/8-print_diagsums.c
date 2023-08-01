d6f19f532c63118508f669c6560893ca292c8fd6
fadirafik <fadihadddad11@gmail.com>
2023-08-01 04:18:20 +0300

    this is an automated commit

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -20,5 +20,5 @@ void print_diagsums(int *a, int size)
 		sum += a[(size * i) + i];
 		sum2 += a[(size * (i + 1)) - (i + 1)];
 	}
-	printf("%d, %d \n", sum, sum2);
+	printf("%d, %d\n", sum, sum2);
 }
