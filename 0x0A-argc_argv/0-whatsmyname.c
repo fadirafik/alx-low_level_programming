fca180210bacd51cca520a3e8c0619dd8da34e92
fadirafik <fadihadddad11@gmail.com>
2023-08-05 02:57:23 +0300

    this is an automated commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-/*
+/**
  * main - prints the name of the program
- * @:argc: number of parameters inserted
- * @:argv: names of the parameters inserted
+ * @argc: number of parameters inserted
+ * @argv: names of the parameters inserted
  *
  * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
