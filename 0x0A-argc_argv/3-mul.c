10bbf32dbd6a083e3be5ec0c82fce70f9d8d82b1
fadirafik <fadihadddad11@gmail.com>
2023-08-05 03:17:48 +0300

    this is an automated commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - multiplies the parameters of the program
+ * @argc: number of parameters inserted
+ * @argv: names of the parameters inserted
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%i\n", argv[1] * argv[2]);
+	return (0);
+}
+
+
