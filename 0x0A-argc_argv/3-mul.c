be1be6e1c26c64ceba4013e94401d5e97636913c
fadirafik <fadihadddad11@gmail.com>
2023-08-05 03:29:29 +0300

    this is an automated commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,10 +8,13 @@
  *
  * Return: 0 if successfull 1 if argc < 2
  */
-int mul(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	if (argc > 3)
+	if (argc != 3)
+	{
+		printf("ERROR\n");
 		return (1);
+	}
 	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
