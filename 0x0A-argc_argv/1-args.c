b4e4adb50e1b5857cb4da87b76f23cb8747506cd
fadirafik <fadihadddad11@gmail.com>
2023-08-05 03:02:41 +0300

    this is an automated commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of parameters in the program
+ * @argc: number of parameters inserted
+ * @argv: names of the parameters inserted
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+        printf("%i\n", argc);
+	(void)argv;
+        return (0);
+}
+
+
