d85ae75dedc736bb6bf1e3dfd04ce30e7a0586ea
fadirafik <fadihadddad11@gmail.com>
2023-08-05 05:05:49 +0300

    this is an automated commit

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - figures out the number of dollar bills to be given
+ * @argc: number of parameters inserted
+ * @argv: names of the parameters inserted
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int money;
+	int change;
+
+	if (argc != 2)
+	{
+		printf("Error\n")
+		return (1);
+	}
+	money = atoi(argv[1]);
+
+	while (money > 0)
+	{
+		change++;
+		if ((money - 25) >= 0)
+		{
+			money -= 25;
+			continue;
+		}
+		if ((money - 10) >= 0)
+		{
+			money -= 10;
+			continue;
+		}
+		if ((money - 5) >= 0)
+		{
+			money -= 5;
+			continue;
+		}
+		if ((money - 2) >= 0)
+		{
+			money -= 2;
+			continue;
+		}
+		money--;
+	}
+	printf("%d\n", money);
+	return (0);
+}
+
