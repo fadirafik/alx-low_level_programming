525e225238878e6b7d1a0b521b7706e9390fafa7
fadirafik <fadihadddad11@gmail.com>
2023-08-05 03:24:15 +0300

    this is an automated commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - multiplies the parameters of the program
  * @argc: number of parameters inserted
  * @argv: names of the parameters inserted
  *
- * Return: Always 0
+ * Return: 0 if successfull 1 if argc < 2
  */
 int main(int argc, char *argv[])
 {
-	printf("%i\n", argv[1] * argv[2]);
+	if (argc > 3)
+		return (1);
+	printf("%i\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }
-
 
