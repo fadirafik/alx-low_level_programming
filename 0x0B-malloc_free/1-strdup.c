b6dede634f7c7d76f46283c68382638da848a1d7
fadirafik <fadihadddad11@gmail.com>
2023-08-09 02:56:38 +0300

    this is an automated commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - copies a string and returns a pointer to the copy
+ * @str: is the string given by the user
+ *
+ * Return: Null if malloc fails or a pointer to the copied string
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *cp;
+	int c;
+
+	while (str[i] != '\0')
+		i++;
+	cp = malloc(i * (sizeof(char)));
+	for (c = 0; c < i; c++)
+		cp[c] = str[c];
+	if (str == NULL || cp == NULL)
+		return (NULL);
+	return (cp);
+}
+
