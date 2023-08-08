3bbe34cc5e874fa64b3ab8ac9912d58c0aae3960
fadirafik <fadihadddad11@gmail.com>
2023-08-09 02:34:28 +0300

    this is an automated commit

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,11 +12,13 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
+	unsigned int i;
 
 	s = malloc(size * sizeof(char));
-	s[0] = c;
 	if (s == NULL || size == 0)
 		return (0);
+	for (i = 0; i < size; i++)
+		s[i] = c;
 	return (s);
 }
 
