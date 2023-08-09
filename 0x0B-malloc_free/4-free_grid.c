1fd35eb9cd260eee986c96fc7ead4263a9ea03aa
fadirafik <fadihadddad11@gmail.com>
2023-08-09 04:45:23 +0300

    this is an automated commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees grid of memory alloc
+ * @grid: user input
+ * @height: height of grid
+ *
+ * Return: nothing
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
+
