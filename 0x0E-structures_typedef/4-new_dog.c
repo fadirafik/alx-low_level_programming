0fdd40003f1861b910cd673afcf2379ef521bd76
fadirafik <fadihadddad11@gmail.com>
2023-08-15 04:30:31 +0300

    this is an automated commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include "dog.h"
 
+int len(char *s);
+char *cop(char *s1, char *s2);
+
 /**
  * new_dog - creates a new instance of a new dog
  * @name: name of the dog
@@ -12,15 +15,69 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *d;
+	dog_t *d;
+	int namel = 0, ownl = 0;
 
-	d = malloc(sizeof(struct dog));
-	if (d == NULL)
-		return (NULL);
+	if (name != NULL && owner != NULL)
+	{
+		namel = len(name);
+		ownl = len(owner);
+		d = malloc(sizeof(dog_t));
+		if (d == NULL)
+			return (NULL);
 
-	d->name = name;
+		d->name = malloc(sizeof(char) * namel);
+		if (d->name == NULL)
+		{
+			free(d);
+			return (NULL);
+		}
+		d->owner = malloc(sizeof(char) * ownl);
+		if (d->owner == NULL)
+		{
+			free(d->name);
+			free(d);
+			return (NULL);
+		}
+	d->name = cop(name, d->name);
 	d->age = age;
-	d->owner = owner;
+	d->owner = cop(owner, d->owner);
+	}
 	return (d);
 }
 
+/**
+ * len - ff
+ * @s: dd
+ *
+ * Return: len
+*/
+int len(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+		i++;
+	i++;
+	return (i);
+}
+
+/**
+ * cop - gag
+ * @s1: ss
+ * @s2: dd
+ *
+ * Return: sf
+*/
+char *cop(char *s1, char *s2)
+{
+	int i;
+
+	while (s1[i] != '\0')
+	{
+		s2[i] = s1[i];
+		i++;
+	}
+	s2[i] = '\0';
+	return (s2);
+}
