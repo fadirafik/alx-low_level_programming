c248f4eb2436be03fdd7193b9ffde05c5c80302a
fadirafik <fadihadddad11@gmail.com>
2023-08-17 01:36:22 +0300

    this is an automated commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int long i = 0;
 
-	if (array != NULL)
+	if (array != NULL || action != NULL)
 	{
 		while (i < size)
 		{
