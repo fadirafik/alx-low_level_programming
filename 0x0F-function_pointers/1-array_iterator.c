b12b6a92e83d36d60e462f432c552ae9428409cd
fadirafik <fadihadddad11@gmail.com>
2023-08-17 01:31:30 +0300

    this is an automated commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,23 +2,23 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator
+ * array_iterator - iterates through an array
  * @array: input of an array
  * @size: size of the array
  * @action: is the function needed to work
  *
- * Return: nothing  
+ * Return: nothing
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-    long unsigned int i = 0;
+	unsigned int long i = 0;
 
-    if (array != NULL)
-    {
-        while (i < size)
-        {
-            action(*(array + i));
-            i++;
-        }
-    }
+	if (array != NULL)
+	{
+		while (i < size)
+		{
+			action(*(array + i));
+			i++;
+		}
+	}
 }
