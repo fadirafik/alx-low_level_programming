3dfafeb56f31345e3d1c5e4b91e4d1cd84a33e62
fadirafik <fadihadddad11@gmail.com>
2023-08-17 01:08:41 +0300

    this is an automated commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,8 @@
 #ifndef function_poiters_h
 #define function_pointers_h
-void print_name(char *name, void (*f)(char *));
-void print_name_as_is(char *name);
-void print_name_uppercase(char *name);
+#include <stdlib.h>
 
+void print_name(char *name, void (*f)(char *));
 
 
 
