79dad371176a46da6d00a7caf272c22b97ffcb3c
fadirafik <fadihadddad11@gmail.com>
2023-08-17 05:03:01 +0300

    this is an automated commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -20,11 +20,6 @@ int main(int argc, char *argv[])
 	}
 	if (si == '+' || si == '-' || si == '*' || si == '/' || si == '%')
 	{
-		if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
-		{
-			printf("Error\n");
-			exit(100);
-		}
 		first = atoi(argv[1]);
 		second = atoi(argv[3]);
 		oper =  get_op_func(argv[2])(first, second);
