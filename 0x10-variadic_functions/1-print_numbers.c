542c4e657c503fd7c3dc785899422c85e232276c
fadirafik <fadihadddad11@gmail.com>
2023-08-18 02:10:35 +0300

    this is an automated commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,19 +13,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 
 	va_start(args, n);
-	if (separator != NULL)
+
+	for (i = 0; i < ((int)n) ; i++)
 	{
-		for (i = 0; i < ((int)n - 1); i++)
-		{
-			printf("%d%s", va_arg(args, int), separator);
-		}
 		printf("%d", va_arg(args, int));
+
+		if (separator != NULL && i != ((int)n - 1))
+		{
+			printf("%s", separator);
+		}
 	}
-	else
-	{
-		for (i = 0; i < ((int)n); i++)
-			printf("%d", va_arg(args, int));
-	}
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
