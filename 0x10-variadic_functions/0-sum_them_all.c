5975993d35e12dfdb8954ac3164bbd50a8a32de0
fadirafik <fadihadddad11@gmail.com>
2023-08-17 20:15:43 +0300

    this is an automated commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sums up all the arguments
+ * @n: number of arguments
+ *
+ * Return: sum of all the arguments
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	int sum = 0;
+	int i;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < (int)n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,6 @@
+#ifndef _HEAD_
+#define _HEAD_
+
+int sum_them_all(const unsigned int n, ...);
+
+#endif
