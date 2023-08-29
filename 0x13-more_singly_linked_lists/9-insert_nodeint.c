125af5341389a244d81f2b6e31a7340f77aa5f89
fadirafik <fadihadddad11@gmail.com>
2023-08-29 16:34:24 +0300

    this is an automated commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -20,6 +20,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	}
 	new->n = n;
+	new->next = NULL;
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
 	while (temp && i < (idx - 1))
 	{
