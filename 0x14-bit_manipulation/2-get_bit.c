42cadf7cc5f8bf28c9d7e24e8fede8390555ef2f
fadirafik <fadihadddad11@gmail.com>
2023-09-01 02:39:27 +0300

    this is an automated commit

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -10,10 +10,11 @@
 */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	if (index > 64)
+		return (-1);
 	if (n & (1 << index))
 		return (1);
 	else if (!(n & (1 << index)))
 		return (0);
-	else
-		return (-1);
+	return (-1);
 }
