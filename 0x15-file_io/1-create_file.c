555b632f94b9f410943fe6d9250d1a625410f826
fadirafik <fadihadddad11@gmail.com>
2023-09-05 03:58:04 +0300

    this is an automated commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,9 +13,12 @@ int create_file(const char *filename, char *text_content)
 	int succ;
 	mode_t mode = 0600;
 
+	if (!filename)
+		return (-1);
+
 	fp = open(filename, O_CREAT | O_RDWR | O_TRUNC, mode);
 
-	if (fp < 0 || !filename)
+	if (fp < 0)
 	{
 		close(fp);
 		return (-1);
