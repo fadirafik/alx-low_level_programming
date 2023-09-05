ec8bc5333f6c859b25e02c376c4b43cb25ee9d4a
fadirafik <fadihadddad11@gmail.com>
2023-09-05 04:37:43 +0300

    this is an automated commit

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -16,13 +16,15 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_APPEND);
+	if(fd < 0)
+		return (-1);
 
 	if (text_content)
 	{
 		while (text_content[len])
 			len++;
 		w = write(fd, text_content, len);
-		if (fd < 0 || w < 0)
+		if (w < 0)
 			return (-1);
 	}
 	close(fd);
