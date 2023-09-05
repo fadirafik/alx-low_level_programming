aace8a329cbb7512cb783ae87653ef9926bbe135
fadirafik <fadihadddad11@gmail.com>
2023-09-05 04:05:41 +0300

    this is an automated commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,6 @@ int create_file(const char *filename, char *text_content)
 
 	if (fp < 0)
 	{
-		close(fp);
 		return (-1);
 	}
 
@@ -33,7 +32,6 @@ int create_file(const char *filename, char *text_content)
 	succ = write(fp, text_content, len);
 	if (succ < 0)
 	{
-		close(fp);
 		return (-1);
 	}
 	close(fp);
