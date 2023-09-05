5c4e3963d6abc7fcc7c7885b36d0d68d21e93b78
fadirafik <fadihadddad11@gmail.com>
2023-09-05 04:29:34 +0300

    this is an automated commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends a given text
+ * to a file
+ * @filename: pointer to the file given
+ * @text_content: text to be written
+ * Return: 1 on success and -1 on failure
+*/
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, w;
+	int len = 0;
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_APPEND);
+	while (text_content[len])
+		len++;
+	w = write(fd, text_content, len);
+	if (fd < 0 || w < 0)
+		return (-1);
+
+	close(fd);
+	return (1);
+}
