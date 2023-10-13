06b78ca59ee843b266dcc59364e5941ff9d1c0c7
fadirafik <fadihadddad11@gmail.com>
2023-10-13 19:43:17 +0300

    this is an automated commit

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -4,6 +4,7 @@
  * sum_dlistint - sums up all the nodes of a
  * doubly linked list
  * @head: a pointer to the head of a doubly linked list
+ * Return: sum of all the nodes
 */
 int sum_dlistint(dlistint_t *head)
 {
