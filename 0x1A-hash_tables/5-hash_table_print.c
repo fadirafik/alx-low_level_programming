81b79d2cb7d6fa5e8afb2768f8a45107c86cb981
fadirafik <fadihadddad11@gmail.com>
2023-11-03 20:18:59 +0200

    this is an automated commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,46 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints a hash tables
+ * @ht: a pointer to a hash table
+ * Return: nothing
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *temp;
+	int count = 0;
+
+	if (ht == NULL)
+		return;
+
+
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+
+		if (temp == NULL)
+		{
+			continue;
+		}
+		else if (count)
+		{
+			printf(", ");
+		}
+		while (temp)
+		{
+			if (temp->next == NULL)
+			{
+				printf("'%s': '%s'", temp->key, temp->value);
+			}
+			else
+			{
+				printf("'%s': '%s', ", temp->key, temp->value);
+			}
+			temp = temp->next;
+			count++;
+		}
+	}
+	if (count)
+		printf("\n");
+}
