c0c35613ae2e876605ec3a167d136b45873ecd9d
fadirafik <fadihadddad11@gmail.com>
2023-11-03 18:22:41 +0200

    this is an automated commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,22 +2,22 @@
 
 /**
  * hash_table_create - creates a hash table
- * @size: is the size of the hash table 
+ * @size: is the size of the hash table
  * Return: a pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = malloc(sizeof(hash_node_t));
-    unsigned long int i;
+	hash_table_t *table = malloc(sizeof(hash_node_t));
+	unsigned long int i;
 
-    if(table == NULL)
-        return NULL;
-    
-    table->size = size;
-    table->array = malloc(sizeof(hash_node_t)*size);
-    if (table->array == NULL)
-        return (NULL);
-    for(i = 0; i < size; i++)
-        table->array[i] = NULL;
-    return (table);
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t) * size);
+	if (table->array == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+	return (table);
 }
