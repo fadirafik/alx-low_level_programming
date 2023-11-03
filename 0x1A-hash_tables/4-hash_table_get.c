74a41a69d2966f771875a52a5bdff97f32d8f666
fadirafik <fadihadddad11@gmail.com>
2023-11-03 18:04:13 +0200

    this is an automated commit

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - gets a value of an index
+ * from a hash table
+ * @ht: pointer to the hash table
+ * @key: key to the item to be retrieved
+ * Return: value given to the key
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int numkey;
+
+	numkey = key_index((const unsigned char *)key, ht->size);
+	if (ht->array[numkey] == NULL)
+		return (NULL);
+
+	else
+		return (ht->array[numkey]->value);
+}
