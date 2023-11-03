05759404e6c1b7bbef4ba0e69be0406f07bf7967
fadirafik <fadihadddad11@gmail.com>
2023-11-04 01:40:22 +0200

    this is an automated commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,9 +7,9 @@
 */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
+	unsigned long int i;
 	hash_node_t *temp;
-    hash_node_t *next;
+	hash_node_t *next;
 
 
 	if (ht == NULL)
@@ -19,15 +19,15 @@ void hash_table_delete(hash_table_t *ht)
 	{
 		temp = ht->array[i];
 
-        while (temp)
-        {
-            next = temp->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-            temp = next;
-        }
-    }
-    free(ht->array);
-    free(ht);
+		while (temp)
+		{
+			next = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
